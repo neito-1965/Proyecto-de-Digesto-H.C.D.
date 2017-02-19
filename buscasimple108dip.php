c95a09618e27baaddf1988181b390a664603d0a3
neito-1965 <mavrodriguez@yahoo.com>
2017-02-19 13:52:47 -0300

    Add files via upload

---
+++ buscasimple108dip.php
@@ -0,0 +1,250 @@
+<!doctype html> 
+<html lang="en"> 
+<head>
+	<meta charset="UTF-8">
+	<title>DIGESTO - H.C.D.</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta http-equiv="expires" content="timestamp">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/jquery-3.1.1.js"></script>
+	<script src="js/bootstrap.js"></script>
+</head> 
+<?php flush(); ?>
+<body> 
+
+<!-- Menú -->
+	<!-- Static navbar -->
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://www.hcdcorrientes.gov.ar/digesto/legislacion/index.html">Digesto H.C.D.</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="http://www.hcdcorrientes.gov.ar/digesto/legislacion/index.html">Inicio</a></li>
+            <li><a href="rama9.html">Leyes por Materia</a></li>
+            <li><a href="consultas.html">Consultas</a></li>
+        </ul>
+          
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+    <div class="container page-header">
+     
+
+
+<form action="http://www.hcdcorrientes.gov.ar/digesto/legislacion/buscasimple106dip.php" method="post"> 
+
+
+<?
+$NL =utf8_encode($_POST['NL']);
+$Voz =utf8_encode($_POST['Voz']);
+$Subvoz =utf8_encode($_POST['Subvoz']);
+$Extracto =utf8_encode($_POST['Extracto']);
+$Sancion =utf8_encode($_POST['Sancion']);
+$Promulgacion =utf8_encode($_POST['Promulgacion']);
+$Observaciones =utf8_encode($_POST['Observaciones']);
+$PBO =utf8_encode($_POST['PBO']);
+$es =utf8_encode($_POST['es']);
+$materia =utf8_encode($_POST['materia']);
+$nlm =utf8_encode($_POST['nlm']);
+$modi =utf8_encode($_POST['modi']);
+$regla =utf8_encode($_POST['regla']);
+$reglad =utf8_encode($_POST['reglad']);
+$audio =utf8_encode($_POST['audio']);
+$braille =utf8_encode($_POST['braille']);
+$NDL =utf8_encode($_POST['NDL']);
+
+
+	$host="localhost";
+	$usuarioh="neito1965";
+	$claveh="neItoMaVr$1965";
+	$enlace = mysql_connect($host,$usuarioh,$claveh);
+
+	mysql_set_charset('utf8', $enlace);
+	mysql_select_db("legislatura2015", $enlace); 
+
+
+if ($NL<>"") {
+$SQL1="SELECT* FROM leyesnv  WHERE NL LIKE '$NL'  ORDER BY NL ASC";
+
+}if ($Voz<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE Voz LIKE '%$Voz%' ORDER BY NL ASC";
+
+}if ($Subvoz<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE Subvoz LIKE '%$Subvoz%' ORDER BY NL ASC";
+
+}if ($Extracto<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE Extracto LIKE '%$Extracto%' ORDER BY NL ASC";
+
+}if ($Sancion<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE Sancion LIKE '%$Sancion%' ORDER BY NL ASC";
+
+}if ($Promulgacion<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE Promulgacion LIKE '%$Promulgacion%' ORDER BY NL ASC";
+
+}if ($Observaciones<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE Observaciones LIKE '%Observaciones%' ORDER BY NL ASC";
+
+}if ($PBO<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE PBO LIKE '%$PBO%' ORDER BY NL ASC";
+
+}if ($es<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE es LIKE '%$es%' ORDER BY NL ASC";
+
+}if ($materia<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE materia LIKE '%$materia%' ORDER BY NL ASC";
+
+}if ($nlm<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE nlm LIKE '%$nlm%' ORDER BY NL ASC";
+
+}if ($modi<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE modi LIKE '%$modi%' ORDER BY NL ASC";
+
+}if ($regla<>"") {
+$SQL1="SELECT* FROM leyesnv WHERE regla LIKE '%$regla%' ORDER BY NL ASC";
+
+}
+ 
+
+
+$result=mysql_query($SQL1,$enlace);
+
+$numero = mysql_num_rows($result); 
+
+echo '<div class="alert alert-success" role="alert">Se han encontrado '.$numero.' registros de leyes no vigentes (NV)</div>';
+
+
+
+
+if ($row=mysql_fetch_array ($result)){ 
+
+
+while ($field = mysql_fetch_field($result)){ 
+
+echo "<b>$field->name</b>"; 
+
+} 
+
+
+do {
+
+echo '<div class="panel panel-default"><div class="panel-body">';
+
+$valor2=$row["NL"];
+$valor2=implode('-',explode('/',$valor2));
+$valor3=implode('-',explode('/',$valor2));
+
+echo "<h4><a href='http://www.hcdcorrientes.gov.ar/Leyes-texto/Ley$valor2.pdf' target='_BLANK'>Ley Nº ".utf8_decode($row['NL'])." (".utf8_decode($row['estado']).")</a></h4>"; 
+
+//echo '<b>Materia: </b>'.utf8_decode($row["materia"]).'<br>';
+
+echo '<b>Tema y Subtema: </b>'.utf8_decode($row["Voz"]).' - '.utf8_decode($row["Subvoz"]).'<br>'; 
+
+
+
+
+
+
+if ($row['ta'] == 's') {
+       echo "<a href='http://www.hcdcorrientes.gov.ar/digesto/legislacion/textos-actualizados/Ley$valor2.pdf' target='_BLANK'>Texto actualizado de la Ley</a><br>";
+       
+  } else {
+	   echo '<b>Esta Ley no tiene texto actualizado<br></b>';
+	   
+	
+  }
+
+
+
+
+
+if ($row['audio'] == 's') {
+       echo "
+       <audio controls preload>
+       <source src='http://www.hcdcorrientes.gov.ar/digesto/legislacion/audio/$valor2.mp3' type='audio/mpeg'>
+       Tu navegador no soporta este elemento de audio
+       </audio>
+
+       <br>";
+       
+  } else {
+	   echo "<span class='glyphicon glyphicon-volume-off'></span> Ley sin audio<br>";
+	   
+	
+  }
+
+
+
+if ($row['braille'] == 's') {
+       
+       echo "<a href='http://www.hcdcorrientes.gov.ar/digesto/legislacion/braille/$valor2.pdf' target='_BLANK'><span class='glyphicon glyphicon-th'></span> Ley en braille</a><br>";
+  } else {
+	   
+	   echo "<span class='glyphicon glyphicon-th'></span> Ley sin braille<br>";
+	
+  }
+
+
+
+
+
+echo utf8_decode($row["Extracto"]).'<br>';
+
+echo '<b>Sanción: </b>'.utf8_decode($row["Sancion"]).'<br>'; 
+
+echo '<b>Promulgación: </b>'.utf8_decode($row["Promulgacion"]).'<br>'; 
+
+echo '<b>Observaciones: </b>'.utf8_decode($row["Observaciones"]).'<br>';
+
+echo '<b>P.B.O.: </b>'.utf8_decode($row["PBO"]).'<br>'; 
+
+
+
+echo '</div></div>';
+
+ 
+} while ($row = mysql_fetch_array($result)); 
+
+
+
+
+} else { 
+$Rc=$Rc+1
+
+
+?>
+
+	<script language="JavaScript">
+		window.top.location ="http://www.hcdcorrientes.gov.ar/digesto/legislacion/consultas.html";
+		alert("ESTA LEY O DECRETO LEY QUE ESTA CONSULTANDO ESTA VIGENTE");
+	</script>
+
+
+
+
+<?
+
+}
+?>
+ 
+  </form>
+</div>
+
+  <footer class="footer">
+     
+        <p class="text-muted" style="text-align:center">Honorable Cámara de Diputados<br>de la Provincia de Corrientes</p>
+      
+    </footer>
+
+</body>
+</html>
